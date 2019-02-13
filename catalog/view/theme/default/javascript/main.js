845cc1f76665de4178339033969eea8100b5f95f
qtsoul95 <qtsoul95@gmail.com>
2019-02-13 22:40:28 +0700

    Animate scrollTop, logo image

--- catalog/view/theme/default/javascript/main.js
+++ catalog/view/theme/default/javascript/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    awe_backtotop();
+
     /********************************************************
      # Tab
      ********************************************************/
@@ -31,3 +33,31 @@ $(document).ready(function() {
         return false;
     });
 });
+
+/********************************************************
+ # BACKTOTOP
+ ********************************************************/
+function awe_backtotop() {
+    /* Back to top */
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 200, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+} window.awe_backtotop=awe_backtotop;
